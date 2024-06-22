5f96e289751912d906866234634351274cf3a988
Kaan Barmore-Genç <kaan@bgenc.net>
2024-06-22 05:15:54 +0000

    Set up to run test matrix for browsers and bun

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 [![GitHub License](https://img.shields.io/github/license/seriousbug/microfern)](https://github.com/SeriousBug/microfern/blob/main/LICENSE.txt)
 [![Codecov](https://img.shields.io/codecov/c/github/seriousbug/microfern)](https://app.codecov.io/gh/SeriousBug/microfern)
 
-Microfern is a minimal templating engine for JavaScript & TypeScript that works both in browsers and Node. There are no files, no template inheritance, no recursion or complex operations. The template language is inspired by [Nunjucks](https://github.com/mozilla/nunjucks), and should be easy for anyone to write.
+Microfern is a minimal templating engine for JavaScript & TypeScript that works
+in browsers ([Firefox](https://www.mozilla.org/en-US/firefox/new/),
+[Chrome](https://www.google.com/chrome/) and derivatives),
+[Node.js](https://nodejs.org/en), [Edge Runtime](https://edge-runtime.vercel.app), and [Bun](https://bun.sh). There are no files, no template
+inheritance, no recursion or complex operations. The template language is
+inspired by [Nunjucks](https://github.com/mozilla/nunjucks), and should be easy
+for anyone to write.
 
 The whole thing fits within 100 lines of code, and has no dependencies. Add templating support to your project without inflating your bundle size!
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,52 @@
 /// <reference types="vitest" />
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
-import camelCase from 'camelcase'
-import packageJson from './package.json'
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import camelCase from "camelcase";
+import packageJson from "./package.json";
+import { BrowserConfigOptions, VitestEnvironment } from "vitest";
 
-const packageName = packageJson.name.split('/').pop() || packageJson.name
+const packageName = packageJson.name.split("/").pop() || packageJson.name;
+
+const testRuntime = process.env.TEST_RUNTIME || "node";
+console.log("testRuntime", testRuntime);
+
+function getBrowserConfig(): BrowserConfigOptions | undefined {
+  if (testRuntime === "chrome") {
+    return {
+      name: "chrome",
+      headless: true,
+    };
+  }
+  if (testRuntime === "firefox") {
+    return {
+      name: "firefox",
+      headless: true,
+    };
+  }
+}
+
+function getEnvironmentConfig(): VitestEnvironment | undefined {
+  if (testRuntime === "node") {
+    return "node";
+  }
+  if (testRuntime === "edge") {
+    return "edge-runtime";
+  }
+}
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es', 'cjs', 'umd', 'iife'],
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es", "cjs", "umd", "iife"],
       name: camelCase(packageName, { pascalCase: true }),
       fileName: packageName,
     },
   },
-  plugins: [
-    dts({ rollupTypes: true }),
-  ],
-  test: {},
-})
+  plugins: [dts({ rollupTypes: true })],
+  test: {
+    browser: getBrowserConfig(),
+    environment: getEnvironmentConfig(),
+  },
+});
