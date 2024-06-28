53a8ddc5cb3ec70b9e4e5dc4b7b93eb8df27c964
Kaan Barmore-Genç <kaan@bgenc.net>
2024-06-28 04:20:29 +0000

    Add tests for date plugin

--- packages/date/src/date.ts
+++ packages/date/src/date.ts
@@ -1,11 +1,20 @@
 import type { Plugin } from "microfern";
 import * as date from "date-fns";
 
-export const toUnixTimestamp: Plugin = (input: string) =>
-  date.getUnixTime(date.parseISO(input)).toString();
-
-export const fromUnixTimestamp: Plugin = (input: string) =>
-  date.formatISO(date.fromUnixTime(parseInt(input)));
+export const toUnixTimestamp: Plugin = (input: string) => {
+  const parsedDate = date.parseISO(input);
+  if (parsedDate.toString() === "Invalid Date") {
+    throw new Error(`Invalid date: ${input}`);
+  }
+  return date.getUnixTime(parsedDate).toString();
+};
+export const fromUnixTimestamp: Plugin = (input: string) => {
+  const timestamp = parseInt(input);
+  if (Number.isNaN(timestamp)) {
+    throw new Error(`Invalid timestamp: ${input}`);
+  }
+  return date.formatISO(date.fromUnixTime(timestamp));
+};
 
 export const DATE_PLUGINS = {
   toUnixTimestamp,
